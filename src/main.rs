7474493d2190da159ec77ceb952eca009987caa3
Sungjoon Kim <methodtweak@naver.com>
2024-06-14 18:26:03 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use hyper_tls::HttpsConnector;
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, GenericError>;
 
-static URI_WEBHOOK: &str = "https://outlook.office.com/webhook/ec2f0739-7f10-4697-939d-631d93736de5@c6014ab3-8365-4c93-bcd9-972ca2a738b4/IncomingWebhook/f0b9efc40d034ecc8708a9122cfb88cf/881b8a36-96a2-4bab-b37b-b291ad7274cc";
-static SUCCESS_DATA: &str = r#"{ summary: "장애여부 체크", "sections": [ { "activityTitle": "우이 홈페이지 상태 점검", "activitySubtitle": "점검결과", "activityText": "정상" } ] }"#;
-static FAIL_DATA: &str = r#"{ "summary": "장애여부 체크", "sections": [ { "activityTitle": "우이 홈페이지 상태 점검", "activitySubtitle": "점검결과", "activityText": "에러발생" } ] }"#;
+static URI_WEBHOOK: &str = "https://outlook.office.com/webhook/ec2f0739-7f10-4697-939d-631d93736de5@c6014ab3-8365-4c93-bcd9-972ca2a738b4/IncomingWebhook/f0b9efc40d034ecc8708a9122cfb88cf/881b8a36-96a2-4bab-b37b";
+static SUCCESS_DATA: &str = r#"{ summary: "장애여부 체크", "sections": [ { "activityTitle": "XX 홈페이지 상태 점검", "activitySubtitle": "점검결과", "activityText": "정상" } ] }"#;
+static FAIL_DATA: &str = r#"{ "summary": "장애여부 체크", "sections": [ { "activityTitle": "XX 홈페이지 상태 점검", "activitySubtitle": "점검결과", "activityText": "에러발생" } ] }"#;
 
 #[tokio::main]
 async fn main() {
